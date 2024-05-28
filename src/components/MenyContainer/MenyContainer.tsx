6ee63f333f554c280aa372d62289ede062ed4078
Markus Masalkovski <marmas99@gmail.com>
2024-05-28 22:36:30 +0200

    added css for menycontainer

--- src/components/MenyContainer/MenyContainer.tsx
+++ src/components/MenyContainer/MenyContainer.tsx
@@ -1,7 +1,9 @@
+import './MenyContainer.css';
+
 
 export default function MenyContainer() {
   return (
-    <div className="container">
+    <div className="container meny-container-size bg-success">
         <div className="row">
            <h1 className="display-1 text-center">Markus Masalkovski</h1>
         </div>
