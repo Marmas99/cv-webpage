07761c24b31606c4e8e7fb8d810845924a2652d8
Markus Masalkovski <marmas99@gmail.com>
2024-05-28 22:24:43 +0200

    finished layout for meny

--- src/components/MenyContainer/MenyContainer.tsx
+++ src/components/MenyContainer/MenyContainer.tsx
@@ -1,10 +1,26 @@
-import './MenyContainer.css';
 
 export default function MenyContainer() {
   return (
     <div className="container">
         <div className="row">
-            <h1 className="display-1 text-center">Markus Masalkovski</h1>
+           <h1 className="display-1 text-center">Markus Masalkovski</h1>
+        </div>
+        <div className='row'>
+            <div className='col d-flex justify-content-center'>
+                <div className='d-inline-block'>
+                    <p>column 1</p> 
+                </div>
+            </div>
+            <div className='col d-flex justify-content-center'>
+                <div className='d-inline-block'>
+                    <p>column 2</p> 
+                </div>
+            </div>
+            <div className='col d-flex justify-content-center'>
+                <div className='d-inline-block'>
+                    <p>column 3</p>
+                </div>
+            </div>
         </div>
     </div>
   )
