89cfe27e1c8167e28ffb7f7310f608ed3c1c0c16
Markus Masalkovski <marmas99@gmail.com>
2024-05-28 22:12:14 +0200

    added meny container files

---
+++ src/components/MenyContainer/MenyContainer.tsx
@@ -0,0 +1,11 @@
+import './MenyContainer.css';
+
+export default function MenyContainer() {
+  return (
+    <div className="container">
+        <div className="row">
+            <h1 className="display-1 text-center">Markus Masalkovski</h1>
+        </div>
+    </div>
+  )
+}
